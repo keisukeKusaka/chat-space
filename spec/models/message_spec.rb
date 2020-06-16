737933b8a76fa3769cf44b96d2c2f952d9805121
boborabi <65844843+boborabi@users.noreply.github.com>
2020-06-16 23:30:37 +0900

    Create message_spec.rb

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+describe Message do
+  context "#create" do
+    it "メッセージがあれば保存できる" do
+      # message = build(:message, image: nil)
+      # expect(message).to be_valid
+    end
+  end
+  
+  context "#create" do
+    it "画像があれば保存できる" do
+      # message = build(:message, content: nil)
+      # expect(message).to be_valid
+    end
+  end
+
+
+end
