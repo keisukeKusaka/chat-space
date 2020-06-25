37dcfcad4d0124ca78d2522d22075b7e0562f3f6
boborabi <65844843+boborabi@users.noreply.github.com>
2020-06-25 16:34:30 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,48 @@
 $(function(){
+  function buildHTML(message) {
+    console.log("buildHTML実行");
+
+    if (message.image) {
+      var html = `<div class="message-block">
+                    <div class="message-block__info">
+                      <div class="info-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="info-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-block__message">
+                       ${message.content}
+                      <img src=${message.image} >
+                    </div>
+                  </div>`      
+      return html;
+    } else {
+      var html = `<div class="message-block">
+                    <div class="message-block__info">
+                      <div class="info-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="info-date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-block__message">
+                       ${message.content}
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+
+    console.log(イベント発火);
+    console.log(url);
 
     $.ajax({
       url: url,
@@ -10,7 +50,16 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType:false
+      contentType: false
     })
+    .done (function(data){
+      var html = buildHTML(data);
+      console.log("done実行");
+      console.log(html);
+    })
+    .fail(function() {
+      console.log("fail実行");
+      alert("メッセージ送信に失敗しました");
+    });
   });
 });
