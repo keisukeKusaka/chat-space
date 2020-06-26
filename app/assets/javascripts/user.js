517388aa4fe7eecae785d87f8cb5e7a026965630
boborabi <65844843+boborabi@users.noreply.github.com>
2020-06-26 23:28:47 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,20 +1,52 @@
 $(function(){
 
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+    console.log("addUser実行");
+    console.log(html);
+  };
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  };  
+
   $('#user-search-field').on("keyup", function(){
     var input = $('#user-search-field').val();
     console.log("keyup発火");
     console.log(input);
 
     $.ajax({
-    type: 'GET',
-    url: '/users',
-    data: { keyword: input },
-    dataType: 'json'
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
     })
     .done(function(users){
       console.log("done実行");
+      console.log(users);
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function(){
+      alert("通信エラーです。ユーザーが表示できません。");
       console.log("fail実行");
     });
   });
