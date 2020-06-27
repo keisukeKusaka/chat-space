515672273d16273dc5f95ce7733f01aca4363ab2
boborabi <65844843+boborabi@users.noreply.github.com>
2020-06-27 23:05:56 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,6 +9,7 @@ $(function(){
     `;
     $("#user-search-result").append(html);
   };
+
   function addNoUser() {
     let html = `
       <div class="chat-group-user clearfix">
@@ -16,7 +17,20 @@ $(function(){
       </div>
     `;
     $("#user-search-result").append(html);
-  };  
+  };
+
+  function moveToChatMember(user_name, user_id) {
+    console.log('moveToChatMember実行')
+    var html = `
+                <div class = 'chat-group-user'>
+                  <input name = 'group[user_ids][]' type = 'hidden' value = '${user_id}'>
+                  <p class = 'chat-group-user__name'>${user_name}</p>
+                  <div class = 'user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`;
+
+    $('#chat-group-users').append(html);
+    console.log('メンバーに追加したユーザー名→group[user_ids][]');
+  }
 
   $('#user-search-field').on("keyup", function(){
     var input = $('#user-search-field').val();
@@ -46,5 +60,12 @@ $(function(){
 
   $('#user-search-result').on('click', '.chat-group-user__btn', function(){
     console.log("clickイベント発火");
+    var data_id = $('.chat-group-user__btn').data('user-id');
+    var data_name = $('.chat-group-user__btn').data('user-name');
+    console.log(data_id, data_name);
+
+    $('chat-group-user').remove;
+
+    moveToChatMember(data_name, data_id);
   });
 });
