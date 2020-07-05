9fc36bab712e8c28ba32f39bd6dc4e40d20dbc65
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-05 10:08:52 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:boborabi/chat-space.git"
+set :repo_url, "git@github.com:boborabi/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
